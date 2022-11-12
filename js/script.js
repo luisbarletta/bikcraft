75d85707519e95b39de8d423038d9aac027d4c38
luisbarletta <luisfelipe.angelim23@gmail.com>
2022-11-11 23:39:42 -0300

    ...

---
+++ js/script.js
@@ -0,0 +1,20 @@
+const links = document.querySelectorAll(".header-menu a");
+function ativarLink(link){
+    const url = location.href;
+    const href = link.href;
+    if (url.includes(href)){
+        link.classList.add("ativo");
+    }
+}
+links.forEach(ativarLink);
+
+//ativar itens de orçamento
+
+const parametros = new URLSearchParams(location.search);
+function ativarProduto(parametro){
+    const elemento = document.getElementById(parametro);
+    if(elemento){
+        elemento.checked = true;
+    }
+}
+parametros.forEach(ativarProduto);
