52c1f77d3cf32eebfd8ea33b6804112bfe1eedd8
luisbarletta <luisfelipe.angelim23@gmail.com>
2022-11-13 18:37:54 -0300

    ...

--- js/script.js
+++ js/script.js
@@ -18,3 +18,48 @@ function ativarProduto(parametro){
     }
 }
 parametros.forEach(ativarProduto);
+
+//perguntas frequentes
+
+const perguntas = document.querySelectorAll(".perguntas button");
+
+function ativarPergunta(event){
+    const pergunta = event.currentTarget;
+    const controls = pergunta.getAttribute("aria-controls");
+    const resposta = document.getElementById(controls);
+
+    resposta.classList.toggle("ativa");
+    const ativa = resposta.classList.contains("ativa");
+    pergunta.setAttribute("aria-expanded", ativa);
+}
+
+function eventosPergunta(pergunta){
+    pergunta.addEventListener("click", ativarPergunta);
+}
+
+perguntas.forEach(eventosPergunta);
+
+//galeria de bicicletas
+
+const galeria = document.querySelectorAll(".bicicleta-imagens img");
+const galeriaContainer = document.querySelector(".bicicleta-imagens");
+
+function trocarImagem(event){
+    const img = event.currentTarget;
+    const media = matchMedia("(min-width: 920px)").matches;
+    if (media){
+        galeriaContainer.prepend(img);
+    }
+}
+
+function eventosGaleria(img){
+    img.addEventListener("click", trocarImagem);
+}
+
+galeria.forEach(eventosGaleria);
+
+//animação
+
+if(window.SimpleAnime){
+    new SimpleAnime();
+}
